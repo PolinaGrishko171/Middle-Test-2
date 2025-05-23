0cd0ceee105380f27ad10718d3cd52923eda359c
Полина Гришко <?-emailexample.com>
2025-05-23 18:37:46 +0300

    Image model added

--- gallery/models.py
+++ gallery/models.py
@@ -5,3 +5,13 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Image(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/')
+    categories = models.ManyToManyField(Category)
+    created_date = models.DateField(auto_now_add=True)
+    age_limit = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
